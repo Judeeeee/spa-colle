c7417eba420792ba6069e6d80ef2906f8dcf699d
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-02-12 00:43:48 +0900

    現在地情報を使用したチェックインの実装
    - Haversine公式を利用して二点間距離を算出
    - チェックインをtruthyにしたいので、仮の許容範囲を設定

--- app/controllers/checkin_logs_controller.rb
+++ app/controllers/checkin_logs_controller.rb
@@ -1,13 +1,13 @@
 class CheckinLogsController < ApplicationController
   before_action :set_facility
+  before_action :set_current_location
   def index
     @checkin_logs = current_user.checkin_dates_for(@facility)
   end
 
   def create
-    @checkin_log = current_user.check_in(@facility)
-
-    if @checkin_log.save
+    if @facility.within_distance?(@current_lat, @current_lng)
+      current_user.check_in(@facility)
       redirect_to facility_checkin_logs_path(@facility)
     else
       # "チェックイン失敗時処理"
@@ -18,5 +18,10 @@ class CheckinLogsController < ApplicationController
 
   def set_facility
     @facility = Facility.find(params[:facility_id])
+  end
+
+  def set_current_location
+    @current_lat = params[:latitude].to_i
+    @current_lng = params[:longitude].to_i
   end
 end
