035169fdac49da54731c2f97e718c83603165126
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-02-17 18:24:15 +0900

    付近の施設を検索ページのJSを別ファイルに定義した

---
+++ app/javascript/map.js
@@ -0,0 +1,64 @@
+function initMapWithCurrentLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const currentLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        initMap(currentLocation);
+      },
+      function (error) {
+        console.error("エラー", error);
+      }
+    );
+  }
+}
+
+function initMap(center) {
+  const facilities = JSON.parse(
+    document.getElementById("map").dataset.facilities
+  );
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: center,
+    zoom: 16,
+  });
+
+  const currentLocation = new google.maps.Marker({
+    position: center,
+    map: map,
+    title: "現在地",
+    icon: {
+      url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+      scaledSize: new google.maps.Size(38, 31),
+    },
+  });
+
+  const contentString = "<div>" + "ここに施設詳細リンクを表示させる" + "</div>";
+
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+
+  facilities.forEach(function (facility) {
+    const marker = new google.maps.Marker({
+      position: { lat: facility.latitude, lng: facility.longitude },
+      map: map,
+      title: facility.name,
+      icon: {
+        url: document.getElementById("map").dataset.imageUrl,
+        scaledSize: new google.maps.Size(38, 31),
+      },
+    });
+
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+      });
+    });
+  });
+}
+
+document.addEventListener("DOMContentLoaded", initMapWithCurrentLocation);
