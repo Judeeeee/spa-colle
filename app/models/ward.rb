a8f4e710398ce0cc88ec4835f37eb1af26473ca7
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-03-12 17:33:19 +0900

    チェックインでスタンプカードの区名に印がつくようにした

--- app/models/ward.rb
+++ app/models/ward.rb
@@ -1,2 +1,7 @@
 class Ward < ApplicationRecord
+  has_many :facilities
+
+  def visited_by_user?(user)
+    facilities.joins(:checkin_logs).where(checkin_logs: { user_id: user.id }).exists?
+  end
 end
