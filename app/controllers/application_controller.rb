be27dfef6626365432fffae06c4fdad6235ebcbd
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-03-13 10:53:51 +0900

    ブラウザ検証ツールでデバイス操作を可能にした
    https://bootcamp.fjord.jp/questions/1984

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   include SessionsHelper
-  allow_browser versions: :modern
+  # コメントアウトしないとブラウザ検証ツールでデバイス操作ができない。https://bootcamp.fjord.jp/questions/1984
+  # リリース前にはコメントアウトを外す
+  # allow_browser versions: :modern
   before_action :check_logged_in
 
   def check_logged_in
