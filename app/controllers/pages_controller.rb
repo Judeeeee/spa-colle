0b33344bc31b53c8485199035311afcdefdf3ce3
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-03-24 15:11:31 +0900

    トップページから利用規約・プライバシーポリシーページに遷移できなかったため修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :check_logged_in, only: :index
+  skip_before_action :check_logged_in,  only: [ :index, :terms ]
   def index
     @wards = Ward.joins(:facilities).distinct.order("name_kana")
   end
