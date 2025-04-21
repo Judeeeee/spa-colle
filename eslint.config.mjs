39a3cc2e664419b21a42bf8c06ffa7c59b22048f
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-04-21 16:51:04 +0900

    googleというグローバル変数を許可した

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,12 @@ export default defineConfig([
   },
   {
     files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        google: "readonly",
+      },
+    },
   },
   eslintConfigPrettier,
 ]);
