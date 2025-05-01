3944e9d8dfc8100a39e62db62f144d1989a7d420
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-05-01 15:04:38 +0900

    visitしてからmockを呼び出して上書きする

--- spec/system/geolocation_spec.rb
+++ spec/system/geolocation_spec.rb
@@ -10,7 +10,6 @@ RSpec.describe 'Geolocation Error Handling', type: :system, js: true do
     expect(page).to have_selector('h1', text: 'スパコレ')
   end
 
-
   def mock_geolocation_success
     page.execute_script(<<~JS)
       navigator.geolocation.getCurrentPosition = function(success, error) {
@@ -29,37 +28,33 @@ RSpec.describe 'Geolocation Error Handling', type: :system, js: true do
 
   it 'displays the map with the current location when location permission is granted' do
     visit facilities_map_path
-
     mock_geolocation_success
-    expect(page).to have_selector('h1', text: '付近の施設を検索', wait: 5)  # Turboのロードが完全に終わるのを待つ
 
+    expect(page).to have_selector('h1', text: '付近の施設を検索', wait: 5)
     expect(page).to have_selector('#map', visible: true)
   end
 
   it 'displays an alert when location permission is denied' do
+    visit facilities_map_path
     mock_geolocation_error
 
-    visit facilities_map_path
-    expect(page).to have_selector('h1', text: '付近の施設を検索', wait: 5) # Turboのロードが完全に終わるのを待つ"
-
+    expect(page).to have_selector('h1', text: '付近の施設を検索', wait: 5)
     accept_alert '位置情報の使用が許可されなかったため、現在地を取得できませんでした。'
   end
 
   it 'displays the map on the facility page when location permission is granted' do
     visit facility_path(not_check_in_facility)
-
     mock_geolocation_success
-    expect(page).to have_selector('h1', text: '未チェックイン施設', wait: 5)
 
+    expect(page).to have_selector('h1', text: '未チェックイン施設', wait: 5)
     expect(page).to have_selector('#facility-map', visible: true)
   end
 
-  it 'displays an alert on the facility page when location permission is denied on' do
+  it 'displays an alert on the facility page when location permission is denied' do
+    visit facility_path(not_check_in_facility)
     mock_geolocation_error
 
-    visit facility_path(not_check_in_facility)
-
     accept_alert '位置情報の使用が許可されなかったため、現在地を取得できませんでした。'
-    expect(page).to have_selector('h1', text: '未チェックイン施設', wait: 5) # Turboのロードが完全に終わるのを待つ"
+    expect(page).to have_selector('h1', text: '未チェックイン施設', wait: 5)
   end
 end
