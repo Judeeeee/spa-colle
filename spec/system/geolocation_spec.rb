febf0063d971a90cd55e4095ef9bfc65fbcb98d1
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-05-01 15:01:51 +0900

    テスト内で直接JSを実行する形に書き換えた
    allow(...)でJSの挙動をモックしているが、これはRubyレベルのオブジェクトに対するモック
    実際のブラウザ上で実行されるJavaScriptには影響を与えられない

--- spec/system/geolocation_spec.rb
+++ spec/system/geolocation_spec.rb
@@ -3,42 +3,28 @@ require 'rails_helper'
 RSpec.describe 'Geolocation Error Handling', type: :system, js: true do
   let(:user) { create(:user) }
   let!(:not_check_in_facility) { create(:not_check_in_facility) }
-  let(:selenium_driver) { Capybara.current_session.driver.browser } # SeleniumのWebDriverインスタンスを取得
 
   before(:each) do
-    driven_by :selenium_chrome_without_cache # 位置情報確認ダイアログを表示させるために、テスト毎にキャッシュを無効化
+    driven_by :selenium_chrome_without_cache
     login_with_google(user)
     expect(page).to have_selector('h1', text: 'スパコレ')
-    allow(selenium_driver).to receive(:execute_script).and_call_original
   end
 
 
   def mock_geolocation_success
-    allow(selenium_driver).to receive(:execute_script).and_wrap_original do |original, script, *args|
-      if script.include?('navigator.geolocation.getCurrentPosition')
-        original.call(<<~JS, *args)
-          navigator.geolocation.getCurrentPosition = function(success, error) {
-            success({ coords: { latitude: 35.698137, longitude: 139.767935 } });  // not_check_in_facilityの座標
-          };
-        JS
-      else
-        original.call(script, *args)# 地図描画のJSが壊れてしまうので
-      end
-    end
+    page.execute_script(<<~JS)
+      navigator.geolocation.getCurrentPosition = function(success, error) {
+        success({ coords: { latitude: 35.698137, longitude: 139.767935 } });
+      };
+    JS
   end
 
   def mock_geolocation_error
-    allow(selenium_driver).to receive(:execute_script).and_wrap_original do |original, script, *args|
-      if script.include?('navigator.geolocation.getCurrentPosition')
-        original.call(<<~JS, *args)
-          navigator.geolocation.getCurrentPosition = function(success, error) {
-            error({ code: 1 });
-          };
-        JS
-      else
-        original.call(script, *args)# 地図描画のJSが壊れてしまうので
-      end
-    end
+    page.execute_script(<<~JS)
+      navigator.geolocation.getCurrentPosition = function(success, error) {
+        error({ code: 1, message: "User denied Geolocation" });
+      };
+    JS
   end
 
   it 'displays the map with the current location when location permission is granted' do
