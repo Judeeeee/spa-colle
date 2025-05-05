146e84837a207fde0b6993597ea5af428e6fb675
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-05-05 19:11:03 +0900

    スコープに処理を切り出した

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :check_logged_in,  only: [ :index, :terms ]
   def index
-    @wards = Ward.joins(:facilities).distinct.order("name_kana")
+    @wards = Ward.with_facilities_ordered
   end
 
   def terms
--- app/models/ward.rb
+++ app/models/ward.rb
@@ -1,6 +1,10 @@
 class Ward < ApplicationRecord
   has_many :facilities
 
+  scope :with_facilities_ordered, -> {
+    joins(:facilities).includes(:facilities).distinct.order(:name_kana)
+  }
+
   def visited_by_user?(user)
     facilities.joins(:checkin_logs).where(checkin_logs: { user_id: user.id }).exists?
   end
