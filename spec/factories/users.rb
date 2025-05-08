01d5b7c31432f57144a938b08d81b4343110f3af
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-05-08 12:22:42 +0900

    Googleアカウント画像を削除して、ログイン・ログアウトはfooterから行わせる

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,5 @@ FactoryBot.define do
   factory :user do
     name { "Test User" }
     email { "test@example.com" }
-    image { "https://lh3.googleusercontent.com/a/ACg8ocJU5kFyhguKfUQHg_HNx0EWfhjlAq066jpPNweO2xtrsAu-lq" }
   end
 end
