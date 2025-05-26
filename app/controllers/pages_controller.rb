f099cde17b0a5f518325f779402bb0b3f1486c60
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-05-26 21:50:15 +0900

    スタンプカードの区名があいうえお順になっているだけで事足りるので修正

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :check_logged_in,  only: [ :index, :terms, :privacy ]
   def index
-    @wards = Ward.with_facilities_ordered
+    @wards = Ward.ordered_by_kana
     @facility_counts_by_wards = Facility.group(:ward_id).count
     @visited_facility_counts_by_wards = current_user.visited_facility_counts_by_ward if current_user
   end
