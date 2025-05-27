a2cf7da5d9557d88ce6a9e03e51860e48acbc4be
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-05-27 16:13:49 +0900

    見やすくするためにまとまりごとに改行を追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   root "pages#index"
+
   resources :facilities, only: [ :index, :show ] do
     get "map", to: "maps#index", on: :collection
     resources :checkin_logs, only: [ :index, :create ]
   end
+
   resources :users, only: [ :show, :destroy ]
+
   get "/terms", to: "pages#terms"
   get "/privacy", to: "pages#privacy"
+
   get "auth/:provider/callback", to: "sessions#create"
   get "auth/failure", to: "sessions#failure"
   get "log_out", to: "sessions#destroy", as: "log_out"
