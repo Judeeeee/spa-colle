4c0b690410da58d1d6698bb12cd0f4540eb77a43
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-05-28 13:42:05 +0900

    turbo-frame idがDOMにないため落ちていたので見直した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
   def show
+    # showはturbo_streamのリクエスト専用。
+    respond_to do |format|
+      format.turbo_stream { render_user_modal }
+      format.html { redirect_to root_path }
+    end
   end
 
   def destroy
     current_user.destroy
     reset_session
     redirect_to root_path
+  end
+
+  private
+
+  def render_user_modal
+    render turbo_stream: turbo_stream.update(
+      "user-modal-frame",
+      partial: "users/user_modal",
+    )
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -61,9 +61,9 @@ RSpec.describe "Users", type: :system do
         expect(page).to have_selector('h1', text: 'スタンプカード')
 
         click_link "アカウント削除"
-        expect(page).to have_selector('#modal', visible: true, wait: 10)
+        expect(page).to have_selector('#user-modal-frame', wait: 10)
 
-        within "#modal" do
+        within "#user-modal-frame" do
           expect(page).to have_content("アカウント削除")
           click_button "削除する"
         end
