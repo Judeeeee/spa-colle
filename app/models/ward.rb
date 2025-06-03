dd6a59c5865a447cbccb996311f052797db4542d
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-06-03 12:41:21 +0900

    未使用のスコープを削除した

--- app/models/ward.rb
+++ app/models/ward.rb
@@ -2,9 +2,4 @@ class Ward < ApplicationRecord
   has_many :facilities
 
   scope :ordered_by_kana, -> { order(:name_kana) }
-
-  scope :visited_by, ->(user) {
-    joins(facilities: :checkin_logs)
-      .where(checkin_logs: { user_id: user.id })
-  }
 end
