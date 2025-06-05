393b8c1491d4883aae07df5159b79e56f3d3991f
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-06-05 12:10:07 +0900

    Controller内でログイン前後の表示を切り替えた

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :check_logged_in,  only: [ :index, :terms, :privacy ]
   def index
-    @wards = Ward.ordered_by_kana
-    @facility_counts_by_wards = Facility.group(:ward_id).count
-    @visited_facility_counts_by_wards = current_user.visited_facility_counts_by_ward if current_user
+    if current_user
+      @wards = Ward.ordered_by_kana
+      @facility_counts_by_wards = Facility.group(:ward_id).count
+      @visited_facility_counts_by_wards = current_user.visited_facility_counts_by_ward
+      render "after_login"
+    else
+      render "before_login"
+    end
   end
 
   def terms
