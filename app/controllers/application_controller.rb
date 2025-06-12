0cfde0c735ebcac64c2f5901c890b09454d0b2e3
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-06-12 13:26:17 +0900

    本リリースするのでコメントアウトを外した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
   include Authentication
   helper_method :current_user
 
-  # コメントアウトしないとブラウザ検証ツールでデバイス操作ができない。https://bootcamp.fjord.jp/questions/1984
-  # リリース前にはコメントアウトを外す
-  # allow_browser versions: :modern
+  allow_browser versions: :modern
   before_action :check_logged_in
 
   def check_logged_in
