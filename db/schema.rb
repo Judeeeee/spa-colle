3b8509711d27407d138c9865b1cd4ea10da6b296
Judeeeee <43412616+Judeeeee@users.noreply.github.com>
2025-06-12 16:39:16 +0900

    NOT NULL制約を追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_12_072727) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_12_073732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,8 +41,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_12_072727) do
   end
 
   create_table "wards", force: :cascade do |t|
-    t.string "name"
-    t.string "name_kana"
+    t.string "name", null: false
+    t.string "name_kana", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
